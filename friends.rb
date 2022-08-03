7e3045a8e7000cd0750427304c6d3de1179b0335
Patricia Severance <patriciaseverance@gmail.com>
2022-08-03 15:24:26 -0600

    Add places

--- friends.rb
+++ friends.rb
@@ -3,3 +3,8 @@ friends = ["Mikey", "Dana", "Callie", "Sallie", "Mikaela"]
 puts friends[2]
 puts friends[4]
 puts friends[0]
+
+places = ["Belgium", "Greece", "Italy", "Georgia", "Zurich"]
+
+places.push("Denver")
+puts places
